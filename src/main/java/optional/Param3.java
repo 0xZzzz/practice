9d438974a3f75d438ee8479ff0fb97d8332065b5
zhangqiang116 <zhangqiang12@ZB-PC0DYU58.360buyAD.local>
2018-04-19 13:20:15 +0800

    fix

--- src/main/java/optional/Param3.java
+++ src/main/java/optional/Param3.java
@@ -13,4 +13,11 @@ public class Param3 {
     public void setField3(String field3) {
         this.field3 = Optional.ofNullable(field3);
     }
+
+    @Override
+    public String toString() {
+        return "Param3{" +
+                "field3=" + field3 +
+                '}';
+    }
 }
