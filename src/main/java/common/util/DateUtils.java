464e3e1da22da4571bdeaa81e59d14734234875e
zhangqiang116 <zhangqiang12@ZB-PC0DYU58.360buyAD.local>
2018-05-25 14:28:11 +0800

    date utils

--- src/main/java/common/util/DateUtils.java
+++ src/main/java/common/util/DateUtils.java
@@ -1,6 +1,10 @@
-package common.util;
+package com.jd.fw.common.utils;
 
-import java.time.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -8,15 +12,43 @@ import java.time.format.DateTimeFormatter;
  */
 public class DateUtils {
 
+    public static final String NUM_FORMAT = "yyyyMMddHHmmss";
+
+    public static final String MS_NUM_FORMAT = NUM_FORMAT + "SSS";
+
     public static final String COMMON_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-    public static final DateTimeFormatter COMMON_FORMAT_FORMATTER = DateTimeFormatter.ofPattern(COMMON_FORMAT);
+    public static final String MS_COMMON_FORMAT = COMMON_FORMAT + ".SSS";
+
+    public static final DateTimeFormatter COMMON_FORMATTER = DateTimeFormatter.ofPattern(COMMON_FORMAT);
+
+    public static final DateTimeFormatter MS_COMMON_FORMATTER = DateTimeFormatter.ofPattern(MS_COMMON_FORMAT);
+
+    public static final DateTimeFormatter MS_NUM_FORMATTER = DateTimeFormatter.ofPattern(MS_NUM_FORMAT);
 
     /**
      * 格式化毫秒时间戳
      */
     public static String format(long millis) {
-        return COMMON_FORMAT_FORMATTER.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), ZoneId.of("Asia/Shanghai")));
+        return format(millis, COMMON_FORMATTER);
+    }
+
+    /**
+     * 格式化毫秒时间戳
+     */
+    public static String format(long millis, DateTimeFormatter formatter) {
+        return formatter.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), ZoneId.of("Asia/Shanghai")));
+    }
+
+    /**
+     * 解析字符串为毫秒时间戳
+     */
+    public static long parseMillis(String text, String format) {
+        try {
+            return new SimpleDateFormat(format).parse(text).getTime();
+        } catch (ParseException e) {
+            return 0;
+        }
     }
 
 }
