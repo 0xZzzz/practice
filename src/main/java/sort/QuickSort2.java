cf51d45f208b6f9a5bc594a13b31e23f952a3ccb
fansheng.zq <fansheng.zq@alibaba-inc.com>
2020-04-01 15:24:25 +0800

    fix

--- src/main/java/sort/QuickSort2.java
+++ src/main/java/sort/QuickSort2.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 /**
  * 快速排序
+ *
+ * @author 0xZzzz
  */
 public class QuickSort2 {
 
@@ -16,15 +18,15 @@ public class QuickSort2 {
     /**
      * 快速排序
      */
-    public static void quickSort(int[] array, int left, int right) {
+    private static void quickSort(int[] array, int left, int right) {
         if (left >= right) {
             return;
         }
-        //进行第一轮排序获取分割点
+        // 进行第一轮排序获取分割点
         int index = partition(array, left, right);
-        //排序前半部分
+        // 排序前半部分
         quickSort(array, left, index - 1);
-        //排序后半部分
+        // 排序后半部分
         quickSort(array, index + 1, right);
     }
 
@@ -36,9 +38,9 @@ public class QuickSort2 {
      * @param right 数组的后下标
      * @return key的下标index，也就是分片的间隔点
      */
-    public static int partition(int[] array, int left, int right) {
-        //三数取中
-        //下面的两步保证了array[right]是最大的；
+    private static int partition(int[] array, int left, int right) {
+        // 三数取中
+        // 下面的两步保证了array[right]是最大的；
         int mid = left + (right - left) / 2;
         if (array[mid] > array[right]) {
             swap(array, mid, right);
@@ -46,31 +48,32 @@ public class QuickSort2 {
         if (array[left] > array[right]) {
             swap(array, left, right);
         }
-        //接下来只用比较array[left]和array[mid]，让较小的在array[left]位置就OK。
+        // 接下来只用比较array[left]和array[mid]，让较小的在array[left]位置就OK。
         if (array[mid] > array[left]) {
             swap(array, mid, left);
         }
         int key = array[left];
         while (left < right) {
-            //从后半部分向前扫描
+            // 从后半部分向前扫描
             while (array[right] >= key && right > left) {
                 right--;
             }
             array[left] = array[right];
-            //从前半部分向后扫描
+            // 从前半部分向后扫描
             while (array[left] <= key && right > left) {
                 left++;
             }
             array[right] = array[left];
         }
-        array[right] = key;//最后把基准存入
+        // 最后把基准存入
+        array[right] = key;
         return right;
     }
 
     /**
      * 交换
      */
-    public static void swap(int[] arr, int e1, int e2) {
+    private static void swap(int[] arr, int e1, int e2) {
         int tmp = arr[e1];
         arr[e1] = arr[e2];
         arr[e2] = tmp;
